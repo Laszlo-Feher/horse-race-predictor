a3c3021a7ed4ef256ad83e3bbbd04abfde2bca62
Laszlo-Feher <feherlaci99@gmail.com>
2024-05-16 09:22:57 +0200

    running all races ml each method with a different copy of the dataframe

--- machine_learning/machine_learning.py
+++ machine_learning/machine_learning.py
@@ -7,13 +7,26 @@ def learn_and_test(df, target, algorythm, formatted_time):
 
     match algorythm:
         case 'all':
-            # classification_with_bulk_fvs(df, target, formatted_time)
-            # classification_with_individual_results(df, target, formatted_time)
-            # classification_with_equal_results(df, target, formatted_time)
-            # split_to_first_3_and_the_rest(df, target, formatted_time)
-            # classify_by_race_without_conversion(df, target, formatted_time)
-            pairwise_learn_to_rank_pairwise(df, target, formatted_time)
-            return pairwise_learn_to_rank_ndcg(df, target, formatted_time)
+            df_bulk_fvs_copy = df.copy()
+            classification_with_bulk_fvs(df_bulk_fvs_copy, target, formatted_time)
+
+            df_individual_results_copy = df.copy()
+            classification_with_individual_results(df_individual_results_copy, target, formatted_time)
+
+            df_equal_results_copy = df.copy()
+            classification_with_equal_results(df_equal_results_copy, target, formatted_time)
+
+            df_split_copy = df.copy()
+            split_to_first_3_and_the_rest(df_split_copy, target, formatted_time)
+
+            df_race_copy = df.copy()
+            classify_by_race_without_conversion(df_race_copy, target, formatted_time)
+
+            df_pairwise_copy = df.copy()
+            pairwise_learn_to_rank_pairwise(df_pairwise_copy, target, formatted_time)
+
+            df_ndcg_copy = df.copy()
+            return pairwise_learn_to_rank_ndcg(df_ndcg_copy, target, formatted_time)
             # return pairwise_learn_to_rank_map(df, target, formatted_time)
         case "classification_with_bulk_fvs":
             return classification_with_bulk_fvs(df, target, formatted_time)
